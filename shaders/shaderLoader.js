1f21dfe17b557175ad1d84497a2123fa57573f83
Gobal Krishnan V <gobalkrishnan.work@gmail.com>
2026-01-19 11:18:16 +0530

    Refactor ShaderLoader to use IIFE for global exposure

--- shaders/shaderLoader.js
+++ shaders/shaderLoader.js
@@ -1,37 +1,40 @@
-const ShaderLoader = {
-  createShader(gl, type, source) {
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
+(function () {
+  const ShaderLoader = {
+    createShader(gl, type, source) {
+      const shader = gl.createShader(type);
+      gl.shaderSource(shader, source);
+      gl.compileShader(shader);
 
-    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      return shader;
+      if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        return shader;
+      }
+
+      console.error(gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+    },
+
+    createProgram(gl, vertexSource, fragmentSource) {
+      const vs = this.createShader(gl, gl.VERTEX_SHADER, vertexSource);
+      const fs = this.createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+
+      if (!vs || !fs) return null;
+
+      const program = gl.createProgram();
+      gl.attachShader(program, vs);
+      gl.attachShader(program, fs);
+      gl.linkProgram(program);
+
+      if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        return program;
+      }
+
+      console.error(gl.getProgramInfoLog(program));
+      gl.deleteProgram(program);
+      return null;
     }
+  };
 
-    console.error(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-    return null;
-  },
-
-  createProgram(gl, vertexSource, fragmentSource) {
-    const vs = this.createShader(gl, gl.VERTEX_SHADER, vertexSource);
-    const fs = this.createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
-
-    if (!vs || !fs) return null;
-
-    const program = gl.createProgram();
-    gl.attachShader(program, vs);
-    gl.attachShader(program, fs);
-    gl.linkProgram(program);
-
-    if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      return program;
-    }
-
-    console.error(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-    return null;
-  }
-};
-
-export default ShaderLoader;
+  // 👇 expose globally
+  window.ShaderLoader = ShaderLoader;
+})();
